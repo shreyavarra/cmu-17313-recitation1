17fc16f0dfc04333211929de5ec4f898ba84ba7f
Kevin Geng <khg@andrew.cmu.edu>
2020-09-04 14:18:30 -0400

    Update output of HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Shreya Varra says hello, world! This is my third year at CMU!");
+        System.out.println("Amazing!");
     }
 }
